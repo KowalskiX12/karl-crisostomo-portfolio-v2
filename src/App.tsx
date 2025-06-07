40fb9c3302460a89f419941d2611bc7d9caacfdf
Karl Crisostomo <karlmichaelcrisostomo123@gmail.com>
2025-06-07 19:27:36 +0800

    chore: Add Packege and update vite configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { RouterProvider } from "react-router-dom";
-import "./App.css";
-import { CreateAppRouter } from "./lib/routing";
+import { RouterProvider } from 'react-router-dom';
+import '@/styles/App.scss';
+import { CreateAppRouter } from './lib/routing';
+import { NavigationProvider } from './context/NavigationContext';
 
 const router = CreateAppRouter();
 function App() {
   return (
-    <>
+    <NavigationProvider>
+      <div className="background" />
       <RouterProvider router={router} />
-    </>
+    </NavigationProvider>
   );
 }
 
